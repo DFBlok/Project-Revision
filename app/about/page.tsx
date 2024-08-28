976d3c5846b0b5febfd6ee6aea4d7a33a2b8cf46
DFBlok <s219392862@mandela.ac.za>
2024-08-28 12:59:15 +0200

    layout changes

---
+++ app/about/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="mx-auto w-1/2 py-10">
+      <h1 className="text-center mb-10 font-bold pb-3 text-3xl underline bg-purple-600 ">
+        Lorem.
+      </h1>
+      <p className="text-center p-5 text-emerald-950 text-2xl bg-red-200 w-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ullam modi
+        recusandae reiciendis sint enim sit perferendis dignissimos autem
+        necessitatibus, quis aut laborum totam suscipit qui expedita, quam
+        distinctio quisquam!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
